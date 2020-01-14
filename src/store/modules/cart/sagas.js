9ec0e9ee9eea48b94f32b3d06369cb0b421c2204
Paulo Barcelos <Paulo.barcelos@hotmail.com>
2020-01-14 13:43:05 -0300

    adicionando a função que verifica se o produto esta disponivel em estoque.

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -5,9 +5,20 @@ import {formatPrice} from '../../../util/format';
 function* addToCart({id}){
   const productExits = yield select(
     state => state.cart.find(p => p.id === id),
-  )
+  );
+  const stock = yield call(api.get, `/stock/${id}`);
+  
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExits ? productExits.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount  > stockAmount){
+    console.tron.warn ('ERRO');
+    return
+  }
+
   if ( productExits){
-    const amount = productExits.amount + 1;
     yield put(updateAmount(id,amount));
   }else{ const response = yield  call (api.get, `/products/${id}`)
   
