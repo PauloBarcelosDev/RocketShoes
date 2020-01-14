f600bbebb6db29931cb47a955a8346562d56ae08
Paulo Barcelos <Paulo.barcelos@hotmail.com>
2020-01-14 14:22:43 -0300

    redirecionar a tela do carrinho quando o cliente adicionar o produto ao mesmo.

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,6 +1,7 @@
 import {call, select, put, all, takeLatest } from 'redux-saga/effects'
 import {toast} from 'react-toastify';
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { addToCartSucess , updateAmountSuccess } from './actions';
 import {formatPrice} from '../../../util/format';
 function* addToCart({id}){
@@ -31,6 +32,7 @@ function* addToCart({id}){
 
   yield put (addToCartSucess(data));
 }
+history.push('/cart');
 }
 function* updateAmount({id, amount}){
   if (amount <= 0) return ;
